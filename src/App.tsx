9becb46626615ff41dde085289c5a91f93638b4a
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-02-24 16:39:47 +0100

    feat(TASK-04): adding the logic to display pokemon data

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 import PokemonCard from "./components/card/PokemonCard.tsx";
+import { fetchPokemonList, fetchPokemonDetail } from "./services/Pokemon.service.ts";
+import { PokemonDetail, PokemonLight } from "./types/Pokemon.type.ts";
 
 function App() {
+  const [pokemonList, setPokemonList] = useState<PokemonDetail[]>([]);
+
+  useEffect(() => {
+    const loadPokemonData = async () => {
+      const pokemonLightList: PokemonLight[] = await fetchPokemonList(); // Liste avec tout les Pokémon
+
+      // pokemonDetailedList va attendre que toutes les promesses (map la liste des pokémon pour récupérer chaque url) soient résolues
+      const pokemonDetailedList = await Promise.all(
+        pokemonLightList.map((pokemonLight) => fetchPokemonDetail(pokemonLight.url)),
+      );
+      setPokemonList(pokemonDetailedList);
+    };
+
+    loadPokemonData();
+  }, []);
+
   return (
     <>
       <div id="header">
         <img src="/assets/logoPokedex.png" alt="Pokedex" />
       </div>
       <div className="cards">
-        {/* Test avec un pokémon aléatoire pour le moment, ce sera adapté dans l'exo suivant, en lien avec la consigne */}
-        <PokemonCard
-          id={1}
-          name="Pikachu"
-          description="Electric type Pokémon"
-          imageURL="/assets/pikachu.png"
-          types={["Electric"]}
-        />
+        {/* pokemonList récup les infos de pokemonDetailedList via le setPokemonList */}
+        {pokemonList.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.id}
+            id={pokemon.id}
+            name={pokemon.name}
+            description={pokemon.description || "No description available"}
+            imageURL={pokemon.sprites.front_default}
+            types={pokemon.types.map((typeInfo) => typeInfo.type.name)}
+          />
+        ))}
       </div>
     </>
   );
