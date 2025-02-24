042ee909d671418f02b12d8715af9426de60f91c
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-02-24 17:13:15 +0100

    feat(TASK-04): adding CSS for the color's types span

--- src/components/card/PokemonCard.test.tsx
+++ src/components/card/PokemonCard.test.tsx
@@ -10,12 +10,14 @@ describe("Card", () => {
         id={666}
         description="PokemonDescriptionPokemon"
         imageURL="test.png"
-        types={["Ghost"]}
+        types={["ghost", "fire"]}
       />,
     );
 
     expect(screen.getByText("PokemonName")).toBeVisible();
     expect(screen.getByText("#666")).toBeVisible();
+    expect(screen.getByText("ghost")).toBeVisible();
+    expect(screen.getByText("fire")).toBeVisible();
     expect(screen.getByText("PokemonDescriptionPokemon")).toBeVisible();
     expect(screen.getByRole("img")).toHaveAttribute("src", "test.png");
     expect(screen.getByRole("button", { name: "Know More..." })).toBeVisible();
