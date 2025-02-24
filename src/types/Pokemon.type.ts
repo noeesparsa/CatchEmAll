cdfacb75de4c93b57e72784b31fbc891208e6577
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-02-24 14:19:54 +0100

    feat(TASK-04): adding types for the api data

---
+++ src/types/Pokemon.type.ts
@@ -0,0 +1,28 @@
+// Pour la liste elle même des Pokemon
+export type PokemonLight = {
+  name: string;
+  url: string;
+};
+
+// Pour la pagination des Pokemon
+export type PaginatedResult<T> = {
+  count: number;
+  next: string;
+  previous: string;
+  results: T[];
+};
+
+// Récupérer les infos des pokémon
+export type PokemonDetail = {
+  id: number;
+  name: string;
+  description?: string;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+};
