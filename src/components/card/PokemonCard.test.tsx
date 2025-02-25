8f70927c38ca5cc2e8a3cf8f011826c52f17f80f
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-02-25 09:18:00 +0100

    feat(TASK-04): changing the test crash cause of the description deleted & types

--- src/components/card/PokemonCard.test.tsx
+++ src/components/card/PokemonCard.test.tsx
@@ -5,20 +5,13 @@ import PokemonCard from "./PokemonCard";
 describe("Card", () => {
   it("should render the correctly", () => {
     render(
-      <PokemonCard
-        name="PokemonName"
-        id={666}
-        description="PokemonDescriptionPokemon"
-        imageURL="test.png"
-        types={["ghost", "fire"]}
-      />,
+      <PokemonCard name="PokemonName" id={666} imageURL="test.png" types={["GHOST", "FIRE"]} />,
     );
 
     expect(screen.getByText("PokemonName")).toBeVisible();
     expect(screen.getByText("#666")).toBeVisible();
-    expect(screen.getByText("ghost")).toBeVisible();
-    expect(screen.getByText("fire")).toBeVisible();
-    expect(screen.getByText("PokemonDescriptionPokemon")).toBeVisible();
+    expect(screen.getByText("GHOST")).toBeVisible();
+    expect(screen.getByText("FIRE")).toBeVisible();
     expect(screen.getByRole("img")).toHaveAttribute("src", "test.png");
     expect(screen.getByRole("button", { name: "Know More..." })).toBeVisible();
   });
