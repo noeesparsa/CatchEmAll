7794adff4ccb5141c9aed18530084dc215db8293
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-03-03 19:21:24 +0100

    test(TASK-06): changing tests to upgrade the coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,22 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { vi } from "vitest";
 
 import App from "./App";
+import * as pokemonService from "./services/Pokemon.service.ts";
 import { PokemonLight } from "./types/Pokemon.type.ts";
 
 export const mockedPokemonList = [
   {
     name: "bulbasaur",
     url: "https://pokeapi.co/api/v2/pokemon/1/",
+  },
+  {
+    name: "ivysaur",
+    url: "https://pokeapi.co/api/v2/pokemon/2/",
+  },
+  {
+    name: "venusaur",
+    url: "https://pokeapi.co/api/v2/pokemon/3/",
   },
 ] as Array<PokemonLight>;
 
@@ -24,26 +33,53 @@ describe("App", () => {
     expect(screen.getByRole("img", { name: "Pokedex" })).toBeVisible();
   });
 
-  it("should render 'Load More...' button and do the API call when we click on it", () => {
-    const nextUrl = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
-    const loadMorePokemon = vi.fn();
+  it("should render 'Load More...' button", async () => {
+    vi.spyOn(pokemonService, "fetchPokemonList").mockResolvedValue({
+      results: mockedPokemonList,
+      next: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+    });
 
-    render(
-      <div>
-        <div id="header">
-          <img alt="Pokedex" src="/assets/logoPokedex.png" />
-        </div>
-        <div className="cards" />
-        <div className="load__more__button">
-          {nextUrl && <button onClick={loadMorePokemon}>Load More...</button>}
-        </div>
-      </div>,
-    );
+    render(<App />);
 
-    const button = screen.getByRole("button", { name: "Load More..." });
-    expect(button).toBeInTheDocument();
-
-    fireEvent.click(button);
-    expect(loadMorePokemon).toHaveBeenCalled();
+    const button = await screen.findByRole("button", { name: "Load More..." });
+    expect(button).toBeVisible();
   });
+
+  // it("should render a few Pokemon", async () => {
+  //   vi.spyOn(pokemonService, "fetchPokemonDetail").mockResolvedValue(mockedPokemonDetail);
+
+  //   render(<App />);
+
+  //   for (const pokemon of mockedPokemonList) {
+  //     const pokemonName = await screen.findByText(pokemon.name);
+  //     expect(pokemonName).toBeVisible();
+  //   }
+  // });
+
+  // describe("When the user click on 'Load More...' button", () => {
+  //   it("should render the 20 next Pokemon", async () => {
+  //     vi.spyOn(pokemonService, "fetchPokemonList").mockResolvedValue({
+  //       results: mockedPokemonList,
+  //       next: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+  //     });
+
+  //     render(<App />);
+
+  //     const button = await screen.findByRole("button", { name: "Load More..." });
+  //     expect(button).toBeVisible();
+  //   });
+  // });
+
+  // it("should render 'Load More...' button and do the API call when we click on it", () => {
+  //   render(<App />);
+
+  //   const nextUrl = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20";
+  //   const loadMorePokemon = vi.fn();
+
+  //   const button = screen.getByRole("button", { name: "Load More..." });
+  //   expect(button).toBeInTheDocument();
+
+  //   fireEvent.click(button);
+  //   expect(loadMorePokemon).toHaveBeenCalled();
+  // });
 });
