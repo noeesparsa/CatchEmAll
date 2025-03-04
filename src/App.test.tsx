537f08539e080dd143bebe23c6e96fcb53a8bfee
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-03-04 13:29:00 +0100

    test(TASK-06): fix bugs with my tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
 import { vi } from "vitest";
 
 import App from "./App";
@@ -12,11 +12,25 @@ export const mockedPokemonList = [
   },
 ] as Array<PokemonLight>;
 
+export const mockedPokemonListMore = [
+  {
+    name: "pikachu",
+    url: "https://pokeapi.co/api/v2/pokemon/45/",
+  },
+] as Array<PokemonLight>;
+
 export const mockedPokemonDetail = {
   id: 1,
   name: "bulbasaur",
   sprites: { front_default: "bulbasaur.png" },
   types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
+};
+
+export const mockedPokemonDetailMore = {
+  id: 45,
+  name: "pikachu",
+  sprites: { front_default: "pikachu.png" },
+  types: [{ type: { name: "electric" } }, { type: { name: "electric" } }],
 };
 
 describe("App", () => {
@@ -49,25 +63,38 @@ describe("App", () => {
   });
 
   describe("When the user clicks on 'Load More...' button", () => {
-    it("should render the 20 next Pokemon and call loadMorePokemon", async () => {
-      const loadMorePokemon = vi.spyOn(pokemonService, "fetchPokemonList").mockResolvedValue({
-        results: mockedPokemonList,
-        next: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
-      });
+    it("should render the next Pokemon and call loadMorePokemon", async () => {
+      const loadMorePokemon = vi
+        .spyOn(pokemonService, "fetchPokemonList")
+        .mockResolvedValueOnce({
+          results: mockedPokemonList,
+          next: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+        })
+        .mockResolvedValueOnce({
+          results: mockedPokemonListMore,
+          next: "https://pokeapi.co/api/v2/pokemon?offset=40&limit=20",
+        });
+
+      const loadMorePokemonDetails = vi
+        .spyOn(pokemonService, "fetchPokemonDetail")
+        .mockResolvedValueOnce(mockedPokemonDetail)
+        .mockResolvedValueOnce(mockedPokemonDetailMore);
+
+      const expectedPokemonList = [...mockedPokemonList, ...mockedPokemonListMore];
 
       render(<App />);
 
-      for (const pokemon of mockedPokemonList) {
-        const pokemonName = await screen.findByText(pokemon.name);
-        expect(pokemonName).toBeVisible();
-      }
-
       const button = await screen.findByRole("button", { name: "Load More..." });
-      fireEvent.click(button);
-      expect(loadMorePokemon).toHaveBeenCalled();
+      await act(async () => {
+        await fireEvent.click(button);
+      });
+      expect(loadMorePokemon).toHaveBeenCalledTimes(2);
+      expect(loadMorePokemonDetails).toHaveBeenCalledTimes(2);
 
-      for (const pokemon of [...mockedPokemonList, ...mockedPokemonList]) {
-        const pokemonName = await screen.findByText(pokemon.name);
+      console.log(expectedPokemonList);
+
+      for (const pokemon of expectedPokemonList) {
+        const pokemonName = await screen.getByText(pokemon.name);
         expect(pokemonName).toBeVisible();
       }
     });
