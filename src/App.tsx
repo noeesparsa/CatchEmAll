51bb5061ec10fcba8030246c1552d0377d685c9c
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-03-18 16:23:37 +0100

    feat(TASK-07): adding the search bar

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,39 @@ import { PokemonCardInfo, PokemonDetailPage } from "./types/Pokemon.type.ts";
 function App() {
   const [pokemonList, setPokemonList] = useState<PokemonCardInfo[]>([]);
   const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
-    const loadPokemonData = async (): Promise<void> => {
-      try {
-        const { results, next } = await fetchPokemonList();
-        const pokemonDetailedList = await Promise.all(
-          results.map((PokemonList) => fetchPokemonCardInfo(PokemonList.url)),
-        );
-        setPokemonList(pokemonDetailedList);
-        setNextUrl(next);
-      } catch (error) {
-        console.error("Failed to load Pokémon data", error);
-      }
-    };
-
-    loadPokemonData().catch((error) => {
-      console.error("Error in useEffect", error);
-    });
+    loadPokemonData();
   }, []);
+
+  useEffect(() => {
+    if (searchTerm) {
+      searchPokemon(searchTerm);
+    } else {
+      resetPokemonList();
+    }
+  }, [searchTerm]);
+
+  const loadPokemonData = async (): Promise<void> => {
+    try {
+      const { results, next } = await fetchPokemonList();
+      const pokemonDetailedList = await Promise.all(
+        results.map((PokemonList) => fetchPokemonCardInfo(PokemonList.url)),
+      );
+      setPokemonList(pokemonDetailedList);
+      setNextUrl(next);
+    } catch (error) {
+      console.error("Failed to load Pokémon data", error);
+    }
+  };
+
+  const resetPokemonList = async (): Promise<void> => {
+    setPokemonList([]);
+    setNextUrl(null);
+    await loadPokemonData();
+  };
 
   const loadMorePokemon = async (): Promise<void> => {
     try {
@@ -49,8 +63,34 @@ function App() {
     }
   };
 
+  const searchPokemon = async (term: string) => {
+    setIsSearching(true);
+    setPokemonList([]);
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=15000`);
+      const data = await response.json();
+      const filteredResults = data.results.filter((pokemon: { name: string; url: string }) =>
+        pokemon.name.toLowerCase().includes(term.toLowerCase()),
+      );
+      const pokemonDetailedList = await Promise.all(
+        filteredResults.map((PokemonList: { name: string; url: string }) =>
+          fetchPokemonCardInfo(PokemonList.url),
+        ),
+      );
+      setPokemonList(pokemonDetailedList);
+    } catch (error) {
+      console.error("Failed to search Pokémon data", error);
+    } finally {
+      setIsSearching(false);
+    }
+  };
+
   const getPokemonTypes = (pokemon: PokemonCardInfo) => {
     return pokemon.types.map((typeInfo) => typeInfo.type.name);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
   };
 
   return (
@@ -62,6 +102,14 @@ function App() {
             <>
               <div id="header">
                 <img src="./assets/logoPokedex.png" alt="Pokedex" />
+              </div>
+              <div className="search-bar">
+                <input
+                  type="text"
+                  placeholder="Search Pokémon"
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                />
               </div>
               <div className="cards">
                 {pokemonList.map((pokemon) => {
@@ -78,9 +126,11 @@ function App() {
                   );
                 })}
               </div>
-              <div className="load__more__button">
-                {nextUrl && <button onClick={loadMorePokemon}>Load More...</button>}
-              </div>
+              {!isSearching && nextUrl && !searchTerm && (
+                <div className="load__more__button">
+                  <button onClick={loadMorePokemon}>Load More...</button>
+                </div>
+              )}
             </>
           }
         />
