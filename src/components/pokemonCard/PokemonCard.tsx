4b5ee5fd92a668970ab78812c350cf24d317c847
Lorenzo ORFAO <lorenzo.orfao@decathlon.com>
2025-03-18 09:38:39 +0100

    feat(TASK-07): changed the repo name to make it easier to understand

---
+++ src/components/pokemonCard/PokemonCard.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+
+import "./PokemonCard.css";
+import PokemonTypeBadge from "../pokemonTypeBadge/PokemonTypeBadge";
+
+type PokemonCardProps = {
+  name: string;
+  id: number;
+  imageURL: string;
+  types: string[];
+};
+
+const PokemonCard: FC<Readonly<PokemonCardProps>> = ({ name, id, imageURL, types }) => {
+  return (
+    <div className="card">
+      <div className="card__header">
+        <div className="card__header__id">
+          <p>#{id} </p>
+        </div>
+
+        <div className="type__badge">
+          {types.map((type) => (
+            <PokemonTypeBadge key={type} type={type} />
+          ))}
+        </div>
+      </div>
+
+      <div className="card__footer__sprite">
+        <img src={imageURL} alt={name} />
+      </div>
+
+      <div className="card__footer">
+        <div className="card__footer__name">
+          <p>{name}</p>
+        </div>
+        <div className="card__footer__button">
+          <button>Know More...</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonCard;
